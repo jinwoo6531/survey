130a3e6445ea85fd7116d11314da2be15db53972
a.jinwoo6531 <a.meanjintakk6531@gmail.com>
2020-06-11 17:31:22 +0900

    survey

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 var app = express();
 
 // DB setting
@@ -7,7 +8,8 @@ mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
-mongoose.connect(process.env.MONGO_DB);
+
+mongoose.connect("mongodb+srv://jinwoo:1234@cluster0-hpf0t.mongodb.net/<dbname>?retryWrites=true&w=majority");
 var db = mongoose.connection;
 db.once('open', function(){
   console.log('DB connected');
@@ -19,6 +21,49 @@ db.on('error', function(err){
 // Other settings
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname+'/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+// DB schema
+var contactSchema = mongoose.Schema({
+  gender:{type:String},
+  animal:{type:String}
+  
+},
+{
+  collection: 'survey',  //몽구스 콜렉션 이름 지정
+});
+
+var Contact = mongoose.model('contact', contactSchema);
+
+// Routes
+// Home
+app.get('/', function(req, res){
+  res.redirect('/contacts');
+});
+// Contacts - Index
+app.get('/contacts', async function (req, res) {
+  let result = [
+    await Contact.countDocuments({ gender: 'Male',animal:'Tiger' }),
+    await Contact.countDocuments({ gender: 'Male' ,animal:'Elephant'}),
+    await Contact.countDocuments({ gender: 'Female' ,animal:'Tiger'}),
+    await Contact.countDocuments({ gender: 'Female' ,animal:'Elephant'}),
+  ]
+  
+  res.render('contacts/index',{survey:result})
+  
+}),
+// Contacts - New
+app.get('/contacts/new', function(req, res){
+  res.render('contacts/new');
+});
+// Contacts - create
+app.post('/contacts', function(req, res){  
+  Contact.create(req.body, function(err, contact){
+    if(err) return res.json(err);
+    res.redirect('/contacts');
+  });
+});
 
 // Port setting
 var port = 3000;
